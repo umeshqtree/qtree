ac2bc46d6ab7cc5683c3f343e57f474619e6c3ea
umeshqtree <125949602+umeshqtree@users.noreply.github.com>
2023-09-26 20:23:33 +0530

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_s3_bucket" "demo" {
-     bucket = "s3bucket-created-using-terraform-jenkins"
+     bucket = "s3bucket-created-using-terraform-jenkins-job"
 }
 output "bucket_arn" {
     value = aws_s3_bucket.demo.arn
