e5200d484e3bece53f69b698959c6b043290c77d
umeshqtree <125949602+umeshqtree@users.noreply.github.com>
2023-09-27 10:50:45 +0530

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -1,8 +1,11 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "demo" {
-     bucket = "qtreee-demo-bucket-test"
+resource "aws_s3_bucket" "demo-1" {
+     bucket = "qtreee-demo-bucket-test-1"
+}
+resource "aws_s3_bucket" "demo-2" {
+     bucket = "qtreee-demo-bucket-test-2"
 }
 output "bucket_arn" {
     value = aws_s3_bucket.demo.arn
