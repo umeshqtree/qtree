5a8caede2b5b7b82922238abe9fdda2c4dbeb1e7
umeshqtree <umeshbalajis.qtreetechnologies@gmail.com>
2024-06-28 08:52:30 +0530

    adding project files

---
+++ terraform/s3/main.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIAU6GDU5CD4MU356XT"                     //update
+  secret_key = "qt+kHJHgdfsdfdffkiswPsyYqn6PTGr3yCQWiBus8" //update
+  region     = "us-east-2"                                //update
+}
